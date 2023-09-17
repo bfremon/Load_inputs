e16d49ea779aa08c676740d10bc39308854f484c
Benoit FREMON <ben@in.volution.fr>
2023-09-17 14:54:57 +0200

    test files

---
+++ lib_test/hello1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+def hello():
+    sys.stdout.write('Hello 1!\n')
+
+if __name__ == '__main__':
+    hello()
---
+++ lib_test/hello2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+def hello():
+    sys.stdout.write('Hello 2!\n')
+
+if __name__ == '__main__':
+    hello()
