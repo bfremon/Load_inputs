596f96c921ffa996e1f00000b0c432da70957dd9
Benoit FREMON <ben@in.volution.fr>
2024-05-06 21:54:26 +0200

    Added support for sheetname selection

--- Load_inputs/Load_inputs.py
+++ Load_inputs/Load_inputs.py
@@ -24,9 +24,11 @@ def load_inputs(data_path = None, ext = 'csv',
     except:
         raise OSError()
     input_files = scan_by_ext(data_path, ext)
-    ret = conc_input_files(input_files,
-                           sep = sep,
-                           sheet_name = sheet_name)
+    if ext == 'csv':
+        ret = conc_csv_files(input_files,
+                               sep = sep)
+    else:
+        ret = conc_xlsx_files(input_files, sheet_name)
     return ret
 
 
@@ -49,31 +51,100 @@ def scan_by_ext(data_path, ext):
     return ret
 
 
-def conc_input_files(input_files, sep, sheet_name):
-    '''Concatenate input files'''
+def conc_xlsx_files(input_files, sheet_name):
+    '''Concatenate xlsx files'''
+    ret = None
+    n_cols = -1
+    n_inputs = 0
+    datas = _flatten_xlsx(input_files)
+    for f_path in datas:
+        for sheet in datas[f_path]:
+            input_data = datas[f_path][sheet]
+            if sheet_name == None:
+                ret, n_cols = _conc_sheet(ret, input_data, n_cols,
+                                          n_inputs, f_path, sheet)
+            elif sheet_name is not None:
+                if sheet in sheet_name:
+                    ret, n_cols = _conc_sheet(ret, input_data, n_cols,
+                                n_inputs, f_path, sheet)
+                else:
+                    sys.stdout.write('%s: %s dropped%s'
+                                     % (f_path, sheet, os.linesep))
+        n_inputs += 1
+    if ret is None:
+        sys.stdout.write('No data returned with %s sheet_name selection'
+                         % (sheet_name))
+    return ret
+
+
+def conc_csv_files(input_files, sep):
+    '''Concatenate csv files'''
     ret = None
     n_cols = -1
     n_inputs = 0
     for f_path in input_files:
         ext = os.path.basename(f_path).split('.')[-1]
-        if ext == 'csv':
-            input_data = pd.read_csv(f_path, sep = sep)
-        else:
-            input_data = pd.read_excel(f_path,
-                                       sheet_name = sheet_name)
-            if sheet_name is None and len(input_data) == 1:
-                unique_key = list(input_data)[0]
-                input_data = input_data[ unique_key ]
-        if n_inputs == 0:
-            n_cols = len(input_data.columns)
-        elif len(input_data.columns) != n_cols:
-            raise ValueError('Wrong number of columns in %s' % f_path)
-        input_data.insert(len(input_data.columns),
-                          'orig',
-                          [ f_path for i in range(len(input_data)) ])
-        if ret is None:
-            ret = input_data
-        else:
-            ret = pd.concat([ret, input_data])
+        input_data = pd.read_csv(f_path, sep = sep)
+        n_cols = _chk_add_orig_col(input_data, n_cols,
+                                   n_inputs, f_path)
+        ret = _conc_data(ret, input_data)
         n_inputs += 1
+    return ret
+
+
+def _flatten_xlsx(input_files):
+    '''Flatten excel input files for sorting by wanted sheetnames'''
+    ret = {}
+    for f_path in input_files:
+        if not f_path in ret:
+            ret[f_path] = {}
+        datas = pd.read_excel(f_path, sheet_name = None)
+        for sheet in datas:
+            ret[f_path][sheet] = datas[sheet]
+    return ret
+
+
+def _conc_sheet(agg_data, input_data, n_cols, n_inputs, f_path, sheet):
+    '''Concatenate sheet in input_data and add orig_sheet column'''
+    ret = agg_data
+    n_cols =_chk_add_orig_col(input_data, n_cols, n_inputs, f_path)
+    _add_orig_col(input_data, label = sheet,
+                  col_lab = 'orig_sheet')
+    ret = _conc_data(ret, input_data)
+    return (ret , n_cols)
+
+
+def _chk_add_orig_col(input_data, n_cols, n_inputs, f_path):
+    '''Check that number of columns is equal to other input_datas
+    and add orig_col column'''
+    ret = _chk_cols_nb(input_data, n_cols,
+                          n_inputs, f_path)
+    _add_orig_col(input_data, f_path, 'orig_path')
+    return ret
+
+
+def _chk_cols_nb(input_data, n_cols, n_inputs, f_path):
+    '''Check that the number of columns is equal
+    to other data_inputs'''
+    if n_inputs == 0:
+        return len(input_data.columns)
+    elif len(input_data.columns) != n_cols:
+         raise ValueError('Wrong number of columns in %s' % f_path)
+    return n_cols
+
+
+def _add_orig_col(input_data, label, col_lab):
+    '''Add orig_col column with input file path'''
+    input_data.insert(len(input_data.columns),
+                      col_lab,
+                      [ label for i in range(len(input_data)) ])
+
+    
+def _conc_data(agg_data, chunk_data):
+    '''Aggregate chunk_data to agg_data'''
+    ret = None
+    if agg_data is None:
+        ret = chunk_data
+    else:
+        ret = pd.concat([ agg_data, chunk_data ])
     return ret
--- Load_inputs/test_load_inputs.py
+++ Load_inputs/test_load_inputs.py
@@ -10,6 +10,9 @@ class test_load_inputs(unittest.TestCase):
                                   'tmp_test')
     data_path = os.path.join(test_path,
                              'data')
+    sheet_name_removed_path = os.path.join(test_path,
+                                           'sheet_name_selection')
+
     def setUp(self):
         if not os.path.exists(self.test_path):
             os.mkdir(self.test_path)
@@ -36,20 +39,50 @@ class test_load_inputs(unittest.TestCase):
         os.chmod(self.data_path, 0o600)
 
 
-    def mk_input_files(self, ext = 'csv', n_files = 2):
-        self._mk_data_path()
+    def mk_input_files(self, dst_path = None, ext = 'csv',
+                       n_files = 2, n_sheets = None):
+        if dst_path is None:
+            dst_path = self.data_path
+        self._mk_data_path(path = dst_path)
         input_datas = {}
         input_paths = {}
-        for i in range(n_files):
-            input_datas[i] = pd.DataFrame({'a': [ i, ], 'b': [ i + 1, ]})
+        n_datas = 0
+        if n_sheets is None:
+            n_datas = n_files
+        else:
+            if len(n_sheets) != n_files:
+                raise ValueError('len(n_sheets) should be equal to n_files')
+            for n_sheet in n_sheets:
+                n_datas += n_sheet
+        for i in range(n_datas):
+            input_datas[i] = pd.DataFrame({'a': [ i, ],
+                                           'b': [ i + 1, ]})
         if ext == 'csv':       
             for k in input_datas:
-                input_paths[k] = os.path.join(self.data_path, '%s.csv' % k)
+                input_paths[k] = os.path.join(dst_path,
+                                              '%s.csv' % k)
                 input_datas[k].to_csv(input_paths[k], sep = ';')
         else:
-            for k in input_datas:
-                input_paths[k] = os.path.join(self.data_path, '%s.xlsx' % k)
-                input_datas[k].to_excel(input_paths[k])
+            sheet_cnt = 0
+            if n_sheets is not None:
+                input_cnt = 0
+                for n_sheet in n_sheets:
+                    input_path = os.path.join(dst_path,
+                                              '%s.xlsx' % input_cnt)
+                    with pd.ExcelWriter(input_path) as writer:
+                        for i in range(n_sheet):
+                            input_paths[sheet_cnt] = input_path 
+                            sh_name = 'Sheet %i' % int(i)
+                            input_datas[sheet_cnt].to_excel(writer, 
+                                                      sheet_name = sh_name)
+                            sheet_cnt += 1
+                    input_cnt += 1
+                assert sheet_cnt == n_datas
+            else:
+                for k in input_datas:
+                    input_paths[k] = os.path.join(dst_path,
+                                                  '%s.xlsx' % k)
+                    input_datas[k].to_excel(input_paths[k])
         conc_data = pd.concat([ input_datas[k] for k in input_datas ])
         return (conc_data, input_paths)
     
@@ -57,26 +90,27 @@ class test_load_inputs(unittest.TestCase):
     def test_csv(self):
         conc_data = self.mk_input_files()[0]
         out_data = load_inputs(self.data_path)
-        out_data.drop(columns = ['orig', 'Unnamed: 0'],
-                      inplace = True)
+        self._drop_orig_cols(out_data)
         out_data = out_data.sort_values(by = 'a', axis = 'rows')
         self.assertTrue(conc_data.equals(out_data))
 
 
     def test_multiple_csv(self):
-        conc_data = self.mk_input_files(n_files = 20)[0]
+        conc_datas = self.mk_input_files(n_files = 20)
+        conc_data = conc_datas[0]
         out_data = load_inputs(self.data_path)
-        out_data.drop(columns = ['orig', 'Unnamed: 0'],
-                      inplace = True)
+        self._drop_orig_cols(out_data)
         out_data = out_data.sort_values(by = 'a', axis = 'rows')
         self.assertTrue(conc_data.equals(out_data))
 
+        
     def _add_col(self, input_paths, input_idx, ext):
         if ext == 'csv':
             data_i = pd.read_csv(input_paths[input_idx], sep = ';')
         else:
             data_i = pd.read_excel(input_paths[input_idx])
-        data_i.insert(len(data_i.columns), 'c', [ 1 for i in range(len(data_i)) ])
+        data_i.insert(len(data_i.columns), 'c',
+                      [ 1 for i in range(len(data_i)) ])
         if ext == 'csv':
             data_i.to_csv(input_paths[input_idx], sep = ';')
         else:
@@ -86,21 +120,23 @@ class test_load_inputs(unittest.TestCase):
     def test_csv_n_cols_different(self):
         conc_data, input_paths = self.mk_input_files()
         self._add_col(input_paths, 1, 'csv')
-        self.assertRaises(ValueError, load_inputs, self.data_path)
+        self.assertRaises(ValueError, load_inputs,
+                          self.data_path)
 
         
     def test_xls_n_cols_different(self):
         ext = 'xlsx'
         conc_data, input_paths = self.mk_input_files(ext = ext)
         self._add_col(input_paths, 1, ext)
-        self.assertRaises(ValueError, load_inputs, self.data_path, ext = ext)
+        self.assertRaises(ValueError, load_inputs,
+                          self.data_path, ext = ext)
 
         
     def _1sheet_excel(self, ext, n_files):
-        conc_data = self.mk_input_files(ext = 'xlsx', n_files = n_files)[0]
+        conc_data = self.mk_input_files(ext = 'xlsx',
+                                        n_files = n_files)[0]
         out_data = load_inputs(self.data_path, ext = ext)
-        out_data.drop(columns = ['orig', 'Unnamed: 0'],
-                      inplace = True)
+        self._drop_orig_cols(out_data)
         out_data = out_data.sort_values(by = 'a', axis = 'rows')
         self.assertTrue(conc_data.equals(out_data))
 
@@ -113,9 +149,53 @@ class test_load_inputs(unittest.TestCase):
         
     def test_multiple_excel(self):
         self._1sheet_excel('xlsx', n_files = 10)
-        
-        
-    def tearDown(self):
+
+
+    def _drop_orig_cols(self, data):
+        for col_lab in ['orig_path', 'orig_sheet', 'Unnamed: 0']:
+            try:
+                data.drop(columns = [col_lab,], 
+                      inplace = True)
+            except KeyError:
+                pass
+
+            
+    def test_multiple_excel_multiple_sheets(self):
+        conc_data, input_paths = self.mk_input_files(ext = 'xlsx',
+                                                     n_files = 3,
+                                        n_sheets = [1, 2, 3])
+        out_data = load_inputs(self.data_path, ext = 'xlsx')
+        self._drop_orig_cols(out_data)
+        out_data = out_data.sort_values(by = 'a', axis = 'rows')
+        self.assertTrue(conc_data.equals(out_data))
+
+        with pd.ExcelWriter(input_paths[2], mode = 'a',
+                            engine = 'openpyxl') as writer:
+            wrong_col_data = pd.DataFrame({'a': [1, ], 'b': [5, ],
+                                           'c': [6, ]})
+            wrong_col_data.to_excel(writer,
+                                    sheet_name = 'wrong_sheet')
+        self.assertRaises(ValueError, load_inputs, self.data_path,
+                          ext = 'xlsx')
+
+    def test_excel_sheet_name_selection(self):
+        conc_data, input_paths = self.mk_input_files(ext = 'xlsx',
+                                                     n_files = 3,
+                                        n_sheets = [1, 2, 3])
+        out_data = load_inputs(self.data_path, ext = 'xlsx', 
+                               sheet_name = [ 'Sheet 0', 'Sheet 1' ])
+        # Creating a directory with same content as above, but with only 2 sheets in
+        # 3rd file, used as correct result
+        sel_conc_data = self.mk_input_files(self.sheet_name_removed_path,
+                                            ext = 'xlsx',
+                                            n_files = 3,
+                                            n_sheets = [1, 2, 2])[0]
+        self._drop_orig_cols(out_data)
+        out_data = out_data.sort_values(by = 'a', axis = 'rows')
+        self.assertTrue(out_data.equals(sel_conc_data))
+
+
+    def _clean_test_path(self, test_path): 
         for root, dirs, files in os.walk(self.test_path):
             for f in files:
                 f_path = os.path.join(root, f)
@@ -124,7 +204,12 @@ class test_load_inputs(unittest.TestCase):
             for d in dirs:
                 d_path = os.path.join(root, d)
                 os.rmdir(d_path)
+
+                
+    def tearDown(self):
+        self._clean_test_path(self.data_path)
+        self._clean_test_path(self.sheet_name_removed_path)
         os.rmdir(self.test_path)
-            
+
 if __name__ == '__main__':
     unittest.main()
